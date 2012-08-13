34eb1a8b6ff71ec972711849421661b70e0ec1e0
Bettina <bettina.shzu@gmail.com>
2012-08-13 20:38:52 +0200

    example of an atrribute reader

---
+++ bettina/8132012.rb
@@ -0,0 +1,19 @@
+class Switch
+  def initialize(state)
+    @state = state
+  end
+  
+  def state
+    @state
+  end
+end
+
+switch = Switch.new(:off)
+if switch.state == :on
+  puts "the switch is on"
+else
+  puts "the switch is off"
+end
+
+# set internal state of the switch to :on
+switch
