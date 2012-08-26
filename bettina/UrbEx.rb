5e49c15d03de1790c31533e9523d420735663f10
Bettina <bettina.shzu@gmail.com>
2012-08-26 23:13:17 +0200

    Stuck on writing tests

--- bettina/UrbEx.rb
+++ bettina/UrbEx.rb
@@ -7,7 +7,11 @@ class Photo
 class User
 
   def initialize(name)
-    #default is a number but user can name themselves
+    #default name is a number 
+  end
+  
+  def rename(name)
+    #user can re-name themselves
   end
   
   def upload(photo)
@@ -23,9 +27,21 @@ class User
   end
 
 class UserTest < Test::Unit::TestCase
+  def test_name
+    1 = User.new("first user")
+    assert 1.name, "create a User named 1"    
+  end
+  
+  def test_rename
+    elfc = User.rename
+  end
 end
 
 class PhotoTest < Test::Unit::TestCase
+  def test_location
+    Gruenau = Photo.new(location)
+    assert
+  end
 end
 
 #Photos should be automatically resized into a thumbnail, be stored full-size up to 2 MG, be located on a map, timestamp for the upload, and be tagged. Photos should only expose tags to Users so that they may be exported as a group.
