8cb834fdf888af580befc77b0f303492fe3d49aa
Bettina <bettina.shzu@gmail.com>
2012-08-27 17:50:57 +0200

    Added Butcher and Animal classes and started writing tests @svenfuchs

--- bettina/vegetarian.rb
+++ bettina/vegetarian.rb
@@ -1,34 +1,73 @@
-class Vegan
+class Animal
   def initialize(name)
     @name = name
   end
   
-  def eat(food_sample)
-    if food_sample == vegetarian?
+  def eat(food)
+  end
+  
+end
+
+class Vegan < Animal
+  def eat(food)
+    if food == :vegetarian
       puts 'yummy!'
     else
       puts "Sorry, I'm vegetarian"
     end
   end
+  
+  def zombify
+    #becomes a zombie when slaughtered
+  end
+end
+
+class Butcher < Animal
+  def eat(food)
+    if food !== :vegetarian
+    puts 'yummy!'
+    else
+    puts "Where's the meat?"
+    end
+  end
+  
+  def slaughter
+    #slaughters animals or vegans? not sure yet
+  end
 end
 
 class Food
-  def initialize(name, vegetarian)
-    @name  = name
-    @vegetarian = vegetarian
-  end
+  attr_reader :vegetarian
 
-  def names
-    @names
-  end  
-
-  def vegetarian?
-    @vegetarian
+  def vegetarian #check if vegetarian or not 
+    :vegetarian
   end
 
 end
 
-bettina = Vegan.new('bettina')
-food_sample = Food.new('tempeh', true)
+m = Butcher.new("M")
+bettina = Vegan.new("bettina")
+food = 
 
-bettina.eat(food_sample)
+bettina.eat(food)
+m.eat(bettina)
+
+class VeganTest < Test::Unit::TestCase
+  def test_name
+    bettina = Vegan.new("Bettina")
+    assert bettina.name, 'create a new Vegan named Bettina'
+  end
+  
+  def test_eat
+    bettina = Vegan.new("Bettina")
+    if food == :vegetarian
+      assert bettina.eats, "bettina etas vegetarian food"
+    end
+  end
+end
+
+class ButcherTest < Test::Unit::TestCase
+  def test_vegetarian
+    
+  end
+end
