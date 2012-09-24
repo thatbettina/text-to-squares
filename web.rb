b335001e159d4771b7490575441caa17cdac0aee
Bettina <bettina.shzu@gmail.com>
2012-09-24 20:55:34 +0200

    oops

--- web.rb
+++ web.rb
@@ -7,14 +7,12 @@ require 'square'
 
 get '/' do
   "
-  <font face="geneva, arial, sans serif">
   <h1>Text to squares</h1>
   <p>Please input a string:</p>
   <form method='post'>
     <textarea name='input'></textarea>
     <input type='submit'>
   </form>
-  </font>
   "
 end
 
