f204524c7f503352af61420870889defe5f6eea6
Bettina <bettina.shzu@gmail.com>
2012-09-24 20:52:07 +0200

    specified font

--- web.rb
+++ web.rb
@@ -7,12 +7,14 @@ require 'square'
 
 get '/' do
   "
+  <font face="geneva, arial, sans serif">
   <h1>Text to squares</h1>
   <p>Please input a string:</p>
   <form method='post'>
     <textarea name='input'></textarea>
     <input type='submit'>
   </form>
+  </font>
   "
 end
 
