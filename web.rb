a1b5f370f018e00294212d74e37fe595a45b18e6
Bettina <bettina.shzu@gmail.com>
2012-10-01 20:08:28 +0200

    starting over in Rails framework

--- web.rb
+++ web.rb
@@ -6,18 +6,10 @@ require 'picture'
 require 'square'
 
 get '/' do
-  erb :template, :locals => { :rows => application.rows }
-  #renders views/index.erb
-  "
-  <h1>Text to squares</h1>
-  <p>Please input a string:</p>
-  <form method='post'>
-    <textarea name='input'></textarea>
-    <input type='submit'>
-  </form>
-  "
+  erb :get_form
 end
 
 post '/' do
-  app = Application.new(params[:input]).render
+  app = Application.new(params[:input])
+  erb :template, :locals => { :rows => application.rows }
 end
