031994246277ea203c2bae2e19d922dea88f37f4
Bettina <bettina.shzu@gmail.com>
2012-10-01 21:53:34 +0200

    Working with models

---
+++ textrails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Textrails::Application.config.secret_token = '819bc51efb85206fc1de9421f53c0de8a60ff8380bc115dc385bd98657f995aed1ddf7742bf2ead9f1d9a7d047130a0a64e83f5a582fd71f0a7d501f292d459c'
